dcc42ff1040fd664879da3d2f3dae131b034ad00
tainasalinoo-source <tainasalinoo@icloud.com>
2026-07-17 12:53:20 -0300

    Create HistoryPage.js

---
+++ src/pages/HistoryPage.js
@@ -0,0 +1,8 @@
+ javascript
+import React from 'react';
+
+function HistoryPage({ atas, onDelete, onSelect }) {
+  return (
+    <div className="page history-page">
+      <div className="card">
+        <h2>Historico de Atas</h2Page;
