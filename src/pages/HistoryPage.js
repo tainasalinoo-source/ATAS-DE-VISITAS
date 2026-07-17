6b34d7d23f4746e1b0c3305e4af5aa821b29ce6e
tainasalinoo-source <tainasalinoo@icloud.com>
2026-07-17 15:22:46 -0300

    Delete src directory

--- src/pages/HistoryPage.js
+++
@@ -1,8 +0,0 @@
- javascript
-import React from 'react';
-
-function HistoryPage({ atas, onDelete, onSelect }) {
-  return (
-    <div className="page history-page">
-      <div className="card">
-        <h2>Historico de Atas</h2Page;
